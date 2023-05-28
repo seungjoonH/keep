81b0f5033a929319d003806c60827f7c0bce55d6
Hamgule <hsj6831@naver.com>
2023-05-28 22:55:01 +0900

    store implemented

--- keep.c
+++ keep.c
@@ -5,14 +5,15 @@
 #include <stdbool.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <math.h>
 
 #define INF_PM INT_MAX
 #define PM_LEN 10
 
 typedef enum {
-  NO_ERROR, PM_UNMATCH, 
-  KP_EXST, STAT_FAIL, FILE_OPN,
-  ERR_LEN
+  NO_ERROR, NO_KEEP, KP_EXST, 
+  PM_UNMATCH, STAT_FAIL, FILE_OPN,
+  NO_UPDT, ERR_LEN
 } ErrorType;
 
 typedef enum {
@@ -27,10 +28,12 @@ char *cmds[CMD_LEN] = {
 };
 
 char *errMsg[ERR_LEN] = {
-  0x0, "unmatched number of parameter",
-  "already managed by keep",
+  0x0, "not in a keep directory",
+  "already in a keep directory",
+  "unmatched number of parameter",
   "failed to access stat",
   "file open error",
+  "nothing to update",
 };
 
 char **ignore;
@@ -50,28 +53,45 @@ int listLen = 0;
 FileData *tracking;
 int trackLen = 0;
 
+unsigned int currentVersion;
+
+int intlen(int i);
 int min(int x, int y);
 int max(int x, int y);
+
+void loadFileList(char *path);
+void listFiles(char *filepath);
+void listDirs(char *dirpath);
+void copyFile(char *des, char *src);
+
 void terminate(ErrorType err);
 int getPmNum(char *cmdStr);
 void visParams(char **params);
+void getIgnores();
+bool beIgnored(char *path);
 void getCommand(int argc, char **argv);
 void freeCommand();
+void loadTrackingfiles();
+void saveTrackingfiles();
+void loadCurrentVersion();
+void saveCurrentVersion();
+void saveNote(char *msg);
 
 void init();
 
-void listFiles(char *filepath);
-void listDirs(char *dirpath);
-void loadTrackingfiles();
-void saveTrackingfiles();
 void track();
 void untrack();
 
-void getIgnores();
-bool beIgnored(char *path);
+void store();
 
 int main(int argc, char **argv) {
   ErrorType err = NO_ERROR;
+
+  if (strcmp(argv[1], "init")) {
+    DIR *dir = opendir(".keep");
+    if (!dir) terminate(NO_KEEP);
+    closedir(dir);
+  }
 
   getIgnores();
   if (argc == 1) terminate(PM_UNMATCH);
@@ -81,120 +101,25 @@ int main(int argc, char **argv) {
     case INIT: init(); break;
     case TRACK: track(); break;
     case UNTRACK: untrack(); break;
+    case STORE: store(); break;
     default: break;
   }
-  freeCommand();
 
+  freeCommand();
   return err;
 }
 
+// utility functions
+int intlen(int i) { return 1 + (i ? (int) log10(i) : 1); }
 int min(int x, int y) { return x < y ? x : y; }
 int max(int x, int y) { return x > y ? x : y; }
 
-void terminate(ErrorType err) {
-  fprintf(stderr, "[ERROR] %s\n", errMsg[err]);
-  exit(err);
-}
-
-int getPmNum(char *cmdStr) {
-  for (int i = 0; i < CMD_LEN; i++)
-    if (!strcmp(cmds[i], cmdStr)) { cmd = i; return pmNums[i]; }
-  return -1;
-}
-
-void visParams(char **params) {
-  printf("Param:\n");
-  for (int i = 0; i < PM_LEN; i++) printf("%s ", params[i]);
-  printf("\n");
-}
-
-void getCommand(int argc, char **argv) {
-  for (int i = 0; i < PM_LEN; i++) {
-    params[i] = (char *) malloc(100);
-    params[i][0] = '\0';
-  }
-  
-  strcpy(cmdStr, argv[1]);
-
-  int paramCnt = getPmNum(cmdStr);
-
-  if (paramCnt == INF_PM) {
-    if (argc < 3) terminate(PM_UNMATCH);
-  }
-  else if (argc != paramCnt + 2) terminate(PM_UNMATCH);
-
-  if (paramCnt) {
-    for (int i = 0; i < min(argc - 2, PM_LEN); i++) {
-      if (argv[2 + i][strlen(argv[2 + i]) - 1] == '/') {
-        argv[2 + i][strlen(argv[2 + i]) - 1] = '\0';
-        strcpy(params[i], argv[2 + i]);
-      }
-      if (strncmp(argv[2 + i], "./", 2))
-        strcpy(params[i], argv[2 + i]);
-      else strcpy(params[i], argv[2 + i] + 2);
-    }
-  }
-}
-
-void freeCommand() {
-  for (int i = 0; i < PM_LEN; i++) free(params[i]);
-}
-
-void init() {
+// file management functions
+void loadFileList(char *path) {
   struct stat st;
-
-  bool alreadExist = !stat(".keep", &st) && S_ISDIR(st.st_mode);
-
-  if (alreadExist) terminate(KP_EXST);
-  else {
-    int result = mkdir(".keep", 0700);
-
-    FILE *lvfp = fopen(".keep/latest-version", "w");
-    FILE *tffp = fopen(".keep/tracking-files", "w");
-    fprintf(lvfp, "0");
-
-    fclose(tffp);
-    fclose(lvfp);
-  }
-}
-
-void loadTrackingFiles() {
-  FILE *tffp = fopen(".keep/tracking-files", "r");
-  if (!tffp) return;
-
-  char line[1000];
-  FileData *arr = NULL;
-  int count = 0;
-
-  while (fgets(line, 1000, tffp)) {
-    if (line[strlen(line) - 1] == '\n')
-      line[strlen(line) - 1] = '\0';
-
-    char *name = strtok(line, " ");
-    char *time = strtok(NULL, " ");
-
-    FileData *temp = realloc(arr, (count + 1) * sizeof(FileData));
-
-    arr = temp;
-    arr[count].filename = (char *) malloc(strlen(name) * sizeof(char));
-    strcpy(arr[count].filename, name);
-    arr[count++].modtime = (time_t) atoi(time);
-  }
-
-  tracking = arr;
-  trackLen = count;
-
-  fclose(tffp);
-}
-
-void saveTrackingFiles() {
-  FILE *tffp = fopen(".keep/tracking-files", "w");
-  if (!tffp) return;
-
-  for (int i = 0; i < trackLen; i++)
-    fprintf(tffp, "%s %lu\n", tracking[i].filename, tracking[i].modtime);
-
-  fclose(tffp);
+  if (stat(path, &st)) terminate(STAT_FAIL);
+  if (S_ISREG(st.st_mode)) listFiles(path);
+  else if (S_ISDIR(st.st_mode)) listDirs(path);
 }
 
 void listFiles(char *filepath) {
@@ -245,13 +170,214 @@ void listDirs(char *dirpath) {
 	closedir(dir);
 }
 
+void copyFile(char *des, char *src) {
+  FILE* srcfp = fopen(src, "rb");
+  FILE* desfp = fopen(des, "wb");
+
+  if (!srcfp || !desfp) terminate(FILE_OPN);
+
+  char buf[BUFSIZ];
+  size_t read;
+
+  while ((read = fread(buf, 1, sizeof(buf), srcfp)) > 0) {
+    fwrite(buf, 1, read, desfp);
+  }
+
+  fclose(srcfp);
+  fclose(desfp);
+}
+
+// keep basic functions
+void terminate(ErrorType err) {
+  fprintf(stderr, "[ERROR] %s\n", errMsg[err]);
+  exit(err);
+}
+
+int getPmNum(char *cmdStr) {
+  for (int i = 0; i < CMD_LEN; i++)
+    if (!strcmp(cmds[i], cmdStr)) { cmd = i; return pmNums[i]; }
+  return -1;
+}
+
+void visParams(char **params) {
+  printf("Param:\n");
+  for (int i = 0; i < PM_LEN; i++) printf("%s ", params[i]);
+  printf("\n");
+}
+
+void getIgnores() {
+  FILE *file = fopen(".keepIgnore", "r");
+  char **arr;
+
+  int count = 0;
+  char line[1000];
+
+  while (fgets(line, 1000, file)) {
+    if (line[strlen(line) - 1] == '\n')
+      line[strlen(line) - 1] = '\0';
+    
+    char *newStr = (char *) malloc(strlen(line) + 2);
+    sprintf(newStr, "./%s", line);
+    char **temp = realloc(arr, (count + 1) * sizeof(char *));
+
+    arr = temp;
+    arr[count++] = newStr;
+  }
+
+  char **temp = realloc(arr, (count + 1) * sizeof(char *));
+  arr = temp;
+  arr[count++] = "./.keep";
+
+  fclose(file);
+
+  ignore = arr;
+  ignoreLen = count;
+}
+
+bool beIgnored(char *path) {
+  for (int i = 0; i < ignoreLen; i++) {
+    char *temp = (char *) malloc((strlen(path) + 2) * sizeof(char));
+    if (strncmp(path, "./", 2)) sprintf(temp, "./%s", path);
+    else strcpy(temp, path);
+
+    struct stat st;
+    stat(ignore[i], &st);
+    
+    bool cond = !strncmp(ignore[i], temp, strlen(ignore[i]));
+    if (temp[strlen(ignore[i]) - 1] != '/') cond = !strcmp(ignore[i], temp);
+
+    if (cond) return true;
+
+    free(temp);
+  }
+  return false;
+}
+
+void getCommand(int argc, char **argv) {
+  for (int i = 0; i < PM_LEN; i++) {
+    params[i] = (char *) malloc(100);
+    params[i][0] = '\0';
+  }
+  
+  strcpy(cmdStr, argv[1]);
+
+  int paramCnt = getPmNum(cmdStr);
+
+  if (paramCnt == INF_PM) {
+    if (argc < 3) terminate(PM_UNMATCH);
+  }
+  else if (argc != paramCnt + 2) terminate(PM_UNMATCH);
+
+  if (paramCnt) {
+    for (int i = 0; i < min(argc - 2, PM_LEN); i++) {
+      if (argv[2 + i][strlen(argv[2 + i]) - 1] == '/') {
+        argv[2 + i][strlen(argv[2 + i]) - 1] = '\0';
+        strcpy(params[i], argv[2 + i]);
+      }
+      if (strncmp(argv[2 + i], "./", 2))
+        strcpy(params[i], argv[2 + i]);
+      else strcpy(params[i], argv[2 + i] + 2);
+    }
+  }
+}
+
+void freeCommand() {
+  for (int i = 0; i < PM_LEN; i++) free(params[i]);
+}
+
+void loadTrackingFiles() {
+  FILE *tffp = fopen(".keep/tracking-files", "r");
+  if (!tffp) return;
+
+  char line[1000];
+  FileData *arr = NULL;
+  int count = 0;
+
+  while (fgets(line, 1000, tffp)) {
+    if (line[strlen(line) - 1] == '\n')
+      line[strlen(line) - 1] = '\0';
+
+    char *name = strtok(line, " ");
+    char *time = strtok(NULL, " ");
+
+    FileData *temp = realloc(arr, (count + 1) * sizeof(FileData));
+
+    arr = temp;
+    arr[count].filename = (char *) malloc(strlen(name) * sizeof(char));
+    strcpy(arr[count].filename, name);
+    arr[count++].modtime = (time_t) atoi(time);
+  }
+
+  tracking = arr;
+  trackLen = count;
+
+  fclose(tffp);
+}
+
+void saveTrackingFiles() {
+  FILE *tffp = fopen(".keep/tracking-files", "w");
+  if (!tffp) return;
+
+  for (int i = 0; i < trackLen; i++)
+    fprintf(tffp, "%s %lu\n", tracking[i].filename, tracking[i].modtime);
+
+  fclose(tffp);
+}
+
+void loadCurrentVersion() {
+  FILE *fp = fopen(".keep/latest-version", "r");
+  
+  char versionStr[100];
+  fscanf(fp, "%s", versionStr);
+
+  currentVersion = (unsigned int) atoi(versionStr);
+  
+  fclose(fp);
+}
+
+void saveCurrentVersion() {
+  FILE *fp = fopen(".keep/latest-version", "w");
+  fprintf(fp, "%d", currentVersion + 1);
+  fclose(fp);
+}
+
+void saveNote(char *msg) {
+  int verlen = strlen(".keep//note") + intlen(currentVersion + 1);
+  char path[verlen + 1];
+  sprintf(path, ".keep/%d/note", currentVersion + 1);
+  
+  FILE *fp = fopen(path, "w");
+  if (!fp) terminate(FILE_OPN);
+
+  fprintf(fp, "%s", msg);
+  fclose(fp);
+}
+
+
+// `init` function
+void init() {
+  struct stat st;
+
+  bool alreadExist = !stat(".keep", &st) && S_ISDIR(st.st_mode);
+
+  if (alreadExist) terminate(KP_EXST);
+  else {
+    int result = mkdir(".keep", 0700);
+
+    FILE *lvfp = fopen(".keep/latest-version", "w");
+    FILE *tffp = fopen(".keep/tracking-files", "w");
+    fprintf(lvfp, "0");
+
+    fclose(tffp);
+    fclose(lvfp);
+  }
+}
+
+// `track` function
 void track() {
   for (int i = 0; i < PM_LEN; i++) {
     if (!strcmp(params[i], "")) break;
-    struct stat st;
-    if (stat(params[i], &st)) terminate(STAT_FAIL);
-    if (S_ISREG(st.st_mode)) listFiles(params[i]);
-    else if (S_ISDIR(st.st_mode)) listDirs(params[i]);
+    loadFileList(params[i]);
   }
 
   loadTrackingFiles();
@@ -260,7 +386,7 @@ void track() {
     bool isExist = false;
     for (int j = 0; j < trackLen; j++) {
       if (!strcmp(fileList[i].filename, tracking[j].filename)) {
-        tracking[j].modtime = fileList[i].modtime; 
+        if (!tracking[j].modtime) tracking[j].modtime = 0; 
         isExist = true; break;
       }
     }
@@ -277,15 +403,13 @@ void track() {
   saveTrackingFiles();
 }
 
+// `untract` function
 void untrack() {
   for (int i = 0; i < PM_LEN; i++) {
     if (!strcmp(params[i], "")) break;
-    struct stat st;
-    if (stat(params[i], &st)) terminate(STAT_FAIL);
-    if (S_ISREG(st.st_mode)) listFiles(params[i]);
-    else if (S_ISDIR(st.st_mode)) listDirs(params[i]);
+    loadFileList(params[i]);
   }
-
+  
   loadTrackingFiles();
 
   FileData *tempTracking = (FileData *) malloc(sizeof(FileData));
@@ -312,39 +436,74 @@ void untrack() {
   saveTrackingFiles();
 }
 
+// `store` function
+void store() {
+  loadCurrentVersion();
+  loadTrackingFiles();
 
-void getIgnores() {
-  FILE *file = fopen(".keepIgnore", "r");
-  char **arr;
-
+  FileData *arr = (FileData *) malloc(trackLen * sizeof(FileData));
   int count = 0;
-  char line[1000];
 
-  while (fgets(line, 1000, file)) {
-    if (line[strlen(line) - 1] == '\n')
-      line[strlen(line) - 1] = '\0';
+  bool update = false;
+  
+  for (int i = 0; i < trackLen; i++) {
+    struct stat st;
+    bool exist = !stat(tracking[i].filename, &st);
+  
+    if (!exist) { update = true; continue; }
+    update |= !tracking[i].modtime || tracking[i].modtime < st.st_mtime;
     
-    char *newStr = (char *) malloc(strlen(line) + 2);
-    sprintf(newStr, "./%s", line);
-    char **temp = realloc(arr, (count + 1) * sizeof(char *));
-
-    arr = temp;
-    arr[count++] = newStr;
+    arr[count].filename = malloc((strlen(tracking[i].filename) + 1) * sizeof(char));
+    strcpy(arr[count].filename, tracking[i].filename);
+    arr[count++].modtime = tracking[i].modtime;
   }
 
-  fclose(file);
+  tracking = arr;
+  trackLen = count;
 
-  ignore = arr;
-  ignoreLen = count;
-}
+  if (!update) terminate(NO_UPDT);
 
-bool beIgnored(char *path) {
-  for (int i = 0; i < ignoreLen; i++) {
-    char *temp = (char *) malloc((strlen(path) + 2) * sizeof(char));
-    if (strncmp(path, "./", 2)) sprintf(temp, "./%s", path);
-    else strcpy(temp, path);
-    if (!strncmp(ignore[i], temp, strlen(ignore[i]))) return true;
-    free(temp);
+  int verlen = intlen(currentVersion + 1);
+
+  char *desdir = (char *) malloc((strlen(".keep/") + verlen) * sizeof(char));  
+  sprintf(desdir, ".keep/%d", currentVersion + 1);
+  
+  mkdir(desdir, 0700);
+  
+  char *src = ".keep/tracking-files";
+  char *des = (char *) malloc((strlen(src) + verlen + 1) * sizeof(char));
+  sprintf(des, "%s/tracking-files", desdir);
+
+  copyFile(des, src);  
+  saveNote(params[0]);
+  
+  char *tgtdir = (char *) malloc((strlen(".keep//target") + verlen) * sizeof(char));
+  sprintf(tgtdir, ".keep/%d/target", currentVersion + 1);
+  mkdir(tgtdir, 0700);
+  
+  for (int i = 0; i < trackLen; i++) {
+    int slen = strlen(tgtdir) + strlen(tracking[i].filename);
+    char *target = (char *) malloc(slen * sizeof(char));
+  
+    sprintf(target, "%s/%s", tgtdir, tracking[i].filename);
+    copyFile(target, tracking[i].filename);
+  
+    free(target);
   }
-  return false;
+
+  free(desdir);
+  free(des);
+  
+  saveCurrentVersion();
+  
+  for (int i = 0; i < trackLen; i++) {
+    struct stat st;
+    stat(tracking[i].filename, &st);
+    tracking[i].modtime = st.st_mtime;
+  }
+
+  printf("stored as version %d\n", currentVersion + 1);
+
+  saveTrackingFiles();
+  
 }
