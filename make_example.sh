d66966ccbccafaa7fcc1cde991c97dc0e0cf47e6
seungjoonH <hsj6831@gmail.com>
2024-07-27 19:43:31 +0200

    Example structure creation file added

---
+++ make_example.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir="target"
+
+mkdir "./${dir}"
+touch "./${dir}/README.md"
+mkdir "./${dir}/ex1"
+touch "./${dir}/ex1/hello.c"
+touch "./${dir}/ex1/main.c"
+mkdir "./${dir}/ex1/data"
+touch "./${dir}/ex1/data/d001"
+touch "./${dir}/ex1/data/d002"
+mkdir "./${dir}/ex2"
+touch "./${dir}/ex2/list1.c"
+touch "./${dir}/ex2/list2.c"
+touch "./${dir}/ex2/list3.c"
